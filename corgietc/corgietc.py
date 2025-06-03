a10803d87c19d340b538e55f6eff9d9b516db2a6
Dmitry Savransky <dsavransky@gmail.com>
2025-06-03 16:02:10 -0400

    script and code updates

--- corgietc/corgietc.py
+++ corgietc/corgietc.py
@@ -45,6 +45,7 @@ class corgietc(Nemati):
 
     def populate_starlightSuppressionSystems_extra(self):
 
+        # add PSFPeak and contrast stability values
         if "PSFpeak" not in self.allowed_starlightSuppressionSystem_kws:
             self.allowed_starlightSuppressionSystem_kws.append("PSFpeak")
         for param_name in [
@@ -82,6 +83,9 @@ class corgietc(Nemati):
                     update_WAs=False,
                     fill="extrapolate",
                 )
+
+            # ensure that CGintSamp is in the system
+            syst["CGintSamp"] = syst.get("CGIintSamp", 0.1)
 
     def populate_scienceInstruments_extra(self):
         """Additional setup for science instruments."""
@@ -155,3 +159,89 @@ class corgietc(Nemati):
 
             else:
                 raise Exception("Instrument name must contain IMAGER or SPEC")
+
+    def construct_cg(self, mode, WA):
+        "Repackage values at a single WA into CGParameters object"
+
+        syst = mode["syst"]
+        lam = mode["lam"]
+
+        radas = ((1 * u.arcsec).to(u.rad)).value
+
+        omegaPSF = (
+            (syst["core_area"](lam, WA) / syst["input_angle_unit_value"] ** 2)
+            .decompose()
+            .value
+        )
+        CGintmpix = (
+            omegaPSF * radas**2
+            / ((syst["CGintSamp"] * syst["lam"] / self.pupilDiam) ** 2)
+        ).decompose().value
+
+        PSFpeakI = syst["PSFpeak"](lam, WA)[0]
+        if "IMG_NFB1_HLC" in mode["Scenario"]:
+            PSFpeakI /= CGintmpix
+
+        CG_PSFarea_sqlamD = omegaPSF / (syst["lam"].to_value(u.m) / radas)**2
+
+        out = fl.CGParameters(
+            CGcoreThruput=syst["core_thruput"](lam, WA)[0],
+            PSFpeakI=PSFpeakI,
+            omegaPSF=omegaPSF,
+            CGintSamp=syst["CGintSamp"],
+            CGradius_arcsec=None,
+            CGdesignWL=lam.to_value(u.m),
+            CGintmpix=CGintmpix,
+            CG_PSFarea_sqlamD=CG_PSFarea_sqlamD,
+            CGintensity=syst["core_mean_intensity"](lam, WA)[0],
+            CG_occulter_transmission=syst["occ_trans"](lam, WA),
+            CGcontrast=syst["core_contrast"](lam, WA),
+        )
+
+        return out
+
+    def Cp_Cb_Csp(self, TL, sInds, fZ, JEZ, dMag, WA, mode, returnExtra=False, TK=None):
+        """Calculates electron count rates for planet signal, background noise,
+        and speckle residuals.
+
+        Args:
+            TL (:ref:`TargetList`):
+                TargetList class object
+            sInds (~numpy.ndarray(int)):
+                Integer indices of the stars of interest
+            fZ (~astropy.units.Quantity(~numpy.ndarray(float))):
+                Surface brightness of local zodiacal light in units of 1/arcsec2
+            JEZ (~astropy.units.Quantity(~numpy.ndarray(float))):
+                Intensity of exo-zodiacal light in units of ph/s/m2/arcsec2
+            dMag (~numpy.ndarray(float)):
+                Differences in magnitude between planets and their host star
+            WA (~astropy.units.Quantity(~numpy.ndarray(float))):
+                Working angles of the planets of interest in units of arcsec
+            mode (dict):
+                Selected observing mode
+            returnExtra (bool):
+                Optional flag, default False, set True to return additional rates for
+                validation
+            TK (:ref:`TimeKeeping`, optional):
+                Optional TimeKeeping object (default None), used to model detector
+                degradation effects where applicable.
+
+
+        Returns:
+            tuple:
+                C_p (~astropy.units.Quantity(~numpy.ndarray(float))):
+                    Planet signal electron count rate in units of 1/s
+                C_b (~astropy.units.Quantity(~numpy.ndarray(float))):
+                    Background noise electron count rate in units of 1/s
+                C_sp (~astropy.units.Quantity(~numpy.ndarray(float))):
+                    Residual speckle spatial structure (systematic error)
+                    in units of 1/s
+
+        """
+
+        # cast sInds to array
+        sInds = np.array(sInds, ndmin=1, copy=copy_if_needed)
+
+        # Star fluxes (ph/m^2/s)
+        flux_star = TL.starFlux(sInds, mode)
+
